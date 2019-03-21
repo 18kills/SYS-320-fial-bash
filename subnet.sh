b7fd902dc35483b78b4215f1358a404b365152b9
Richard T Swierk <18kills.18kills@gmail.com>
2019-03-21 10:25:25 -0400

    Update subnet.sh

--- subnet.sh
+++ subnet.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 ip="${1%.*}"
-dns=$(host )
 for i in $(seq 1 254)
 do
-        dns="$(host $ip)"
+        dns="$(host ${ip}.${i})"
         dns="${dns#*pointer}"
         echo "${ip}.${i} - ${dns}
 done
